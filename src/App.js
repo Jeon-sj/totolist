c38df984678645ff691b3efc89e653013e797f92
demonj <demonj73@gmail.com>
2021-05-04 07:36:33 +0900

    컴포넌트 만들기

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import { createGlobalStyle } from 'styled-components';
+import TodoTemplate from './components/TodoTemplate';
+import TodoHead from './components/TotoHead';
+import TodoList from './components/TodoList';
+import TodoCreate from './components/TodoCreate';
 
 const GlobalStyle = createGlobalStyle`
   body {
-    background: #e9ecef;
+    background: #e9ecef;   
   }
   `;
-
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <div>안녕하세요</div>
+      <TodoTemplate>
+        <TodoHead />
+        <TodoList />
+        <TodoCreate />
+      </TodoTemplate>      
     </>
   
   );
